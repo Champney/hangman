3d4f05a72526d62be6bad45680bce63b3927e784
Champney <davidjchampney@gmail.com>
2023-06-24 15:44:46 +0100

    Checks the input to make sure that it is alphabetical and only one character

--- milestone_2.py
+++ milestone_2.py
@@ -3,5 +3,14 @@ print(word_list)
 
 import random
 word = random.choice(word_list)
-print(word)
+#print(word)
 guess = input()
+
+word_list = list(word)
+if len(guess) == 1 and guess.isalpha() == True:
+    if guess in word_list:
+        print("Good guess!")
+    elif guess not in word_list:
+        print("Try again!")
+else:
+    print("Oops! that is not a valid input")
