a4e3eed8b093897e5bd70f7a7f0fad472702da52
Champney <davidjchampney@gmail.com>
2023-06-24 16:19:36 +0100

    Swaps out the word 'word' for the word 'choice

--- milestone_2.py
+++ milestone_2.py
@@ -2,11 +2,11 @@ word_list = ['mango', 'watermelon', 'kiwi', 'raspberry', 'lime']
 print(word_list)
 
 import random
-word = random.choice(word_list)
-print(word)
+choice = random.choice(word_list)
+print(choice)
 guess = input()
 
-word_expressed_as_list = list(word)
+word_expressed_as_list = list(choice)
 if len(guess) == 1 and guess.isalpha() == True:
     if guess in word_list:
         print("Good guess!")
